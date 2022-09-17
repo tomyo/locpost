c5595d6faeb17af750d21c02addba2b58d99f062
Tomás Hayes <tomas@hayes.com.ar>
2022-09-17 18:35:34 +0200

    Add gun config

---
+++ api/gun-config.js
@@ -0,0 +1,13 @@
+export const config = {
+  localHostUrl: 'http://localhost:8765/gun',
+  remoteHostUrl: 'https://gun-manhattan.herokuapp.com/gun',
+  rootName: 'locpost',
+  messagesChannelName: 'messages',
+  messageSchema: {
+    title: String,
+    latitude: Number,
+    longitude: Number,
+    message: String,
+    date: String, // ISO
+  }
+}
