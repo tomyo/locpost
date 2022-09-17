d828816d5f9e38a5e4393f49a16148d1e7f5c4c3
Tomás Hayes <tomas@hayes.com.ar>
2022-09-17 22:13:12 +0200

    Add useGun and useMessages in api/use-gun.js
    Move gun related external dependencies to /lib folder,
    use-gun.js uses those.

---
+++ api/use-gun.js
@@ -0,0 +1,24 @@
+'use strict'
+
+import '../lib/gun.js';
+import '../lib/sea.js';
+import '../lib/webrtc.js';
+
+let gun;
+
+export function useGun(GUN, {
+  localHostUrl = 'http://localhost:8765/gun',
+  remoteHostUrl = 'https://gun-manhattan.herokuapp.com/gun',
+} = {}) {
+  if (gun) return gun;
+  if (typeof (window.GUN) === 'undefined') throw Error('GUN is not available in global scope');
+
+  gun = window.GUN(localHostUrl, remoteHostUrl);
+  return gun;
+}
+
+export function useMessages({ messagesChannelName = 'messages' } = {}) {
+
+  const gun = useGun();
+  return gun.get(messagesChannelName);
+}
