b3cf64e1317c1cbb264da9199c3330beb0bd17c4
Tomás Hayes <tomas@hayes.com.ar>
2022-09-17 16:36:22 +0200

    Add geolocation prompt functionality

---
+++ index.js
@@ -0,0 +1,18 @@
+import { getCurrentPosition } from './lib/geolocation.js'
+
+const getLocationButton = document.querySelector('#get-location');
+
+getLocationButton.addEventListener('click', async (e) => {
+  if (!navigator.geolocation) {
+    alert("Geolocation not available. Please choose another method.");
+    return;
+  }
+  try {
+    const position = await getCurrentPosition();
+    console.info("Got location:", position.coords)
+    //TODO: go to messages page and display messages
+  }
+  catch (error) {
+    alert(error.message);
+  }
+});
